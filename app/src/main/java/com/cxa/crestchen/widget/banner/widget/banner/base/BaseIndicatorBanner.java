da724a4d9ff52aec1d3b6c6aaf23f863f734db4a
username <281663448@QQ.COM>
2020-07-06 10:17:28 +0800

    2

--- app/src/main/java/com/cxa/crestchen/widget/banner/widget/banner/base/BaseIndicatorBanner.java
+++ app/src/main/java/com/cxa/crestchen/widget/banner/widget/banner/base/BaseIndicatorBanner.java
@@ -47,8 +47,7 @@ public abstract class BaseIndicatorBanner<E, T extends BaseIndicatorBanner<E, T>
     private Class<? extends BaseAnimator> mUnselectAnimClass;
 
     private LinearLayout mLlIndicators;
-
-
+    
     public BaseIndicatorBanner(Context context) {
         super(context);
         initAttrs(context, null);
