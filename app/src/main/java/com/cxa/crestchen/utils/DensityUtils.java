654f4e4ce10060f54044af017d75ddb9108ce0eb
username <281663448@QQ.COM>
2020-08-06 10:47:41 +0800

    2

--- app/src/main/java/com/cxa/crestchen/utils/DensityUtils.java
+++ app/src/main/java/com/cxa/crestchen/utils/DensityUtils.java
@@ -14,8 +14,7 @@ public final class DensityUtils {
     private DensityUtils() {
         throw new UnsupportedOperationException("u can't instantiate me...");
     }
-
-
+    
     /**
      * DisplayMetrics
      *
