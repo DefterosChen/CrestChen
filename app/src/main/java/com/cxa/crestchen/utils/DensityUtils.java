8eb1a09da33d0d86cb162ea5d95846a0e58061c3
username <281663448@QQ.COM>
2020-08-10 15:47:34 +0800

    2

--- app/src/main/java/com/cxa/crestchen/utils/DensityUtils.java
+++ app/src/main/java/com/cxa/crestchen/utils/DensityUtils.java
@@ -102,7 +102,6 @@ public final class DensityUtils {
         float fontScale = ResUtils.getResources().getDisplayMetrics().scaledDensity;
         return (int) (spValue * fontScale + 0.5f);
     }
-
     /**
      * 根据手机的分辨率从 sp 的单位 转成为 px
      *
