e7465b186bb96609be455b200e4b46c61eeb3b61
DefterosChen <281663448@qq.com>
2020-11-12 15:52:10 +0800

    123

--- app/src/main/java/com/cxa/crestchen/utils/DeviceUtils.java
+++ app/src/main/java/com/cxa/crestchen/utils/DeviceUtils.java
@@ -43,6 +43,8 @@ public class DeviceUtils {
     private static boolean sIsTabletValue = false;
     private static final String BRAND = Build.BRAND.toLowerCase();
 
+
+    
     private DeviceUtils() {
         throw new UnsupportedOperationException("u can't instantiate me...");
     }
